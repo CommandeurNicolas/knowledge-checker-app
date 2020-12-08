3a52d31a89d9ba1bb2d621f7be3e995f0423c20e
FayxChance <fayxchancetom@outlook.com>
2020-12-08 22:48:35 +0100

    add services and beginning of auth

--- lib/main.dart
+++ lib/main.dart
@@ -11,31 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      debugShowCheckedModeBanner: false,
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home:Wrapper(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
 
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Wrapper();
-    //eturn Login();
-    // return SignUp();
-  }
-}
