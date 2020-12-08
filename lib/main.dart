2c703163d867635366e384fc1b275df759f79828
FayxChance <fayxchancetom@outlook.com>
2020-12-08 22:08:03 +0100

    ajout et clean

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:knowledge_checker/screens/wrapper.dart';
+import 'screens/Auth/login.dart';
+import 'screens/Auth/signup.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Wrapper();
+    //eturn Login();
+    // return SignUp();
+  }
+}
---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:knowledge_checker/screens/Auth/authenticate.dart';
+
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    /// return home or auth widget
+    return Authenticate();
+  }
+}
