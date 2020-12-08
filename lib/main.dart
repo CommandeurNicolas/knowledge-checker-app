fe40c928c3b42e13fc07ae83ccaf7c0d612fc62a
FayxChance <fayxchancetom@outlook.com>
2020-12-09 00:21:47 +0100

    provider and stream

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:knowledge_checker/models/user.dart';
 import 'package:knowledge_checker/screens/wrapper.dart';
+import 'package:knowledge_checker/services/auth.dart';
 import 'screens/Auth/login.dart';
 import 'screens/Auth/signup.dart';
-
+import 'package:provider/provider.dart';
 void main() {
   runApp(MyApp());
 }
@@ -10,8 +12,11 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return StreamProvider<User>.value(
+      value: AuthService().user,
+      child: MaterialApp(
       home:Wrapper(),
+      ),
     );
   }
 }
